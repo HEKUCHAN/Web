24299b1419052e39b23d8b83bdbd9eefda1c08ab
HEKUCHAN <70084358+HEKUCHAN@users.noreply.github.com>
2021-06-12 17:32:05 +0900

    XSS対策させた

--- src/js/message.js
+++ src/js/message.js
@@ -5,6 +5,14 @@ window.onload = function () {
     var history = document.getElementsByClassName('user-last-message');
 
     var messageHistory = []
+    
+    function escapeHTML(string) {
+        return string.replace(/\&/g, '&amp;')
+            .replace(/\</g, '&lt;')
+            .replace(/\>/g, '&gt;')
+            .replace(/\"/g, '&quot;')
+            .replace(/\'/g, '&#x27');
+    }
 
     function CreateMessage() {
         var today = new Date();
@@ -14,8 +22,8 @@ window.onload = function () {
             messageHistory.push('message')
             history.value = messageHistory[0];
             var date = today.getFullYear() + "/" + (today.getMonth() + 1) + '/' + today.getDate() + "  " + today.getHours() + ":" + today.getMinutes();
-            chat.insertAdjacentHTML('beforeend', '<div class="my-message"><div class="message-case"><div class="my-message">' + message + '</div></div><p class="date">' + date + '</p></div>');
-            chat.insertAdjacentHTML('beforeend', '<div class="person-message"><div class="user-icon"><img src="img/sozai_cman_jp_20210611230506.svg" class="user-icon"></div><div class="person-info"><div class="person-name">サイトの管理人</div><div class="person-inner-message"><p><span>' + message + '</span></p></div><div><p class="date">' + date + '</p></div></div></div>');
+            chat.insertAdjacentHTML('beforeend', '<div class="my-message"><div class="message-case"><div class="my-message">' + escapeHTML(message) + '</div></div><p class="date">' + date + '</p></div>');
+            chat.insertAdjacentHTML('beforeend', '<div class="person-message"><div class="user-icon"><img src="img/sozai_cman_jp_20210611230506.svg" class="user-icon"></div><div class="person-info"><div class="person-name">サイトの管理人</div><div class="person-inner-message"><p><span>' + escapeHTML(message) + '</span></p></div><div><p class="date">' + date + '</p></div></div></div>');
         }
     }
     botton.addEventListener('click',CreateMessage);
